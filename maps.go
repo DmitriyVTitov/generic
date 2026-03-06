835a48266c4a27e76d1ada8593e86c7ed49ac50c
Dmitriy Titov <dtsp@yandex.ru>
2026-03-07 00:02:12 +0300

    review

--- maps.go
+++ maps.go
@@ -3,7 +3,7 @@ package generic
 // MapItem returns the value for the specified key from the map if it exists
 // and the associated value can be asserted to type T. Otherwise, it returns
 // the zero value of type T.
-func MapItem[T any, Key comparable, Value any](m map[Key]Value, key Key) T {
+func MapItem[T any, K comparable, V any](m map[K]V, key K) T {
 	var zero T
 
 	if m == nil {
